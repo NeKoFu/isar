b77993d2242b9dc803188e81b476646ad62a8b6a
David Roume <david.roume@kavacode.com>
2023-06-16 15:15:48 +0200

    fix: web building, add BigInt for id + reactivate web support

--- packages/isar/lib/src/web/open.dart
+++ packages/isar/lib/src/web/open.dart
@@ -46,9 +46,9 @@ Future<Isar> openIsar({
   required bool relaxedDurability,
   CompactCondition? compactOnLaunch,
 }) async {
-  throw IsarError('Please use Isar 2.5.0 if you need web support. '
-      'A 3.x version with web support will be released soon.');
-  /*await initializeIsarWeb();
+  // throw IsarError('Please use Isar 2.5.0 if you need web support. '
+  //     'A 3.x version with web support will be released soon.');
+  await initializeIsarWeb();
   final schemasJson = getSchemas(schemas).map((e) => e.toJson());
   final schemasJs = jsify(schemasJson.toList()) as List<dynamic>;
   final instance = await openIsarJs(name, schemasJs, relaxedDurability)
@@ -68,7 +68,7 @@ Future<Isar> openIsar({
   }
 
   isar.attachCollections(cols);
-  return isar;*/
+  return isar;
 }
 
 Isar openIsarSync({
